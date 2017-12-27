2088d309161c5ca8cb77f3beb67d468af40d0030
walt-PC\walt <waltleeee@gmail.com>
2017-12-27 09:37:10 +0800

    renew

--- python/pythonBlogServerTest/BlogServer/BlogServer/settings.py
+++ python/pythonBlogServerTest/BlogServer/BlogServer/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASE['default']=dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
+
+ALLOWED_HOSTS=['*']
+
+STATIC_ROOT='staticfiles'
+
+DEBUG=False
+
+try:
+    from.local_settins import *
+except ImportError:
+    pass
